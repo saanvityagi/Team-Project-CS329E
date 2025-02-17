68c94e836ab866a22b045730b9230593dd0b0452
saanvityagi <saanvityagi@hotmail.com>
2025-02-17 14:19:04 -0600

    sprint 1 done

---
+++ project/main.gd
@@ -0,0 +1,14 @@
+extends Node
+
+func new_game():
+	$Player.start($StartPosition.position)
+	
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	new_game()
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
